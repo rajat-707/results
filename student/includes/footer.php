a664a5d2ac8d7b9ee798ca3ffc3d4306b39bf447
rajat1234567899 <130656822+rajat1234567899@users.noreply.github.com>
2025-02-23 19:51:17 +0530

    footer.php

--- student/includes/footer.php
+++ student/includes/footer.php
@@ -2,7 +2,7 @@
             <div class="footer-inner bg-white">
                 <div class="row">
                     <div class="col-sm-6">
-                        &copy; <?php echo date('Y')?> Student Grading System - Developed By Sodiq Ahmed
+                        &copy; <?php echo date('Y')?> PTU Results
                     </div>
                     <div class="col-sm-6 text-right">
                         <!-- Designed by <a href="https://colorlib.com">Colorlib</a> -->
