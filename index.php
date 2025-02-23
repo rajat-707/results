96be0a87432ce63753172246453ed0d13c694399
rajat1234567899 <130656822+rajat1234567899@users.noreply.github.com>
2025-02-23 21:45:47 +0530

    index.php

--- index.php
+++ index.php
@@ -64,14 +64,17 @@ error_reporting(0);
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
     <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> 
-</head>
-<body  >
+    <style>
 
-    <div class="sufee-login d-flex align-content-center flex-wrap" style="background-image:url(img/bg.jpg);">
+    </style>
+</head>
+<body>
+
+    <div class="sufee-login d-flex align-content-center flex-wrap " style="background-image:url(img/bg.jpg);">
         <div class="container">
             <img src="img/bg.jpg">
         </div>
-    </div>
+    
     <div class="login-content">
                 <div class="login-logo">
                     <a href="/">
@@ -98,7 +101,7 @@ error_reporting(0);
             </form>
         </div>
 </div>
-
+</div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
